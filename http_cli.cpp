0767e4a5df8ff598ea3a3df957b2d60c4eef7e36
xxx0624 <xingzheng.cs@gmail.com>
2020-04-25 22:02:05 -0700

    fix int conversion

--- http_cli.cpp
+++ http_cli.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
-#include <algorithm>
-#include <string>
+#include <arpa/inet.h>
+#include <iostream>
+#include <typeinfo>
+#include <sstream>
 using namespace std;
 
 typedef pair<string, string> hostname_path;
@@ -22,13 +29,13 @@ hostname_path_port parse(string url){
     }
     // extract port if it exists
     int port_start_pos = -1, port = -1, port_end_pos = -1;
-    for(int i = hostname_start_pos; i < url.length(); i ++){
+    for(int i = hostname_start_pos; i < (int)url.length(); i ++){
         if(url[i] == '/'){
             break;
         }
         if(url[i] == ':'){ // maybe port exists
             port_start_pos = i + 1;
-            for(int j = i + 1; j < url.length(); j ++){
+            for(int j = i + 1; j < (int)url.length(); j ++){
                 if(url[j] == '/'){
                     break;
                 }
@@ -52,13 +59,13 @@ hostname_path_port parse(string url){
     }
     // extract path
     int path_start_pos;
-    for(path_start_pos = max(hostname_start_pos, port_end_pos); path_start_pos < url.length(); path_start_pos ++){
+    for(path_start_pos = max(hostname_start_pos, port_end_pos); path_start_pos < (int)url.length(); path_start_pos ++){
         if(url[path_start_pos] == '/') {
             break;
         }
     }
     string path = "/";
-    if(path_start_pos >= 0 && path_start_pos < url.length() && url[path_start_pos] == '/'){
+    if(path_start_pos >= 0 && path_start_pos < (int)url.length() && url[path_start_pos] == '/'){
         path += url.substr(path_start_pos + 1, url.length() - 1 - path_start_pos);
     }
     // extract hostname
@@ -73,8 +80,26 @@ hostname_path_port parse(string url){
     part1.first = hostname;
     part1.second = path;
     res.first = part1;
-    res.second = port;
+    if(port != -1){
+        res.second = port;
+    } else {
+        res.second = 80;
+    }
     return res;
+}
+
+int send_msg(int sockFD, string msg){
+    int nbytes_total = 0;
+    const char* request = msg.c_str();
+    while(nbytes_total < (int)msg.length()){
+        int nbytes_last = send(sockFD, request + nbytes_total, msg.length() - nbytes_total, 0);
+        if(nbytes_last == -1){
+            cerr << "fail to send msg back" << gai_strerror(nbytes_last) << endl;
+            return nbytes_last;
+        }
+        nbytes_total += nbytes_last;
+    }
+    return 0;
 }
 
 int main(int argc, char* argv[]){
@@ -85,9 +110,70 @@ int main(int argc, char* argv[]){
 
     string url = argv[1];
     hostname_path_port res = parse(url);
-    cout << "hostname:" << res.first.first << endl;
-    cout << "path:" << res.first.second << endl;
-    cout << "port:" << res.second << endl;
+    // cout << "hostname:" << res.first.first << endl;
+    // cout << "path:" << res.first.second << endl;
+    // cout << "port:" << res.second << endl;
+    string hostname = res.first.first;
+    string path = res.first.second;
+    int port = res.second;
+
+    int error, sockFD;
+    struct addrinfo hints, *addrs, *it;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if((error = getaddrinfo(hostname.c_str(), to_string(port).c_str(), &hints, &addrs)) != 0){
+        cerr << "fail to get addr[" << hostname << "] info, " << gai_strerror(error) << endl;
+        return EXIT_FAILURE;
+    }
+
+    for(it = addrs; it != NULL; it = it->ai_next){
+        if((sockFD = socket(it->ai_family, it->ai_socktype, it->ai_protocol)) == -1){
+            cout << "fail to create socket, " << gai_strerror(sockFD) << endl;
+            continue;
+        }
+        if((error = connect(sockFD, it->ai_addr, it->ai_addrlen)) == -1){
+            close(sockFD);
+            cout << "fail to connect, " << gai_strerror(error) << endl;
+            continue;
+        }
+        //successfully
+        break;
+    }
+
+    if(it == NULL){
+        cout << "fail to connect to the server[" << hostname << "]" << endl;
+        return EXIT_FAILURE;
+    }
+
+    // prepare the request
+    stringstream reqs;
+    reqs << "GET " << path << " " << "HTTP/1.1\r\n";
+    reqs << "Host: " << hostname << "\r\n";
+    reqs << "COnnection: close" << "\r\n";
+    reqs << "\r\n";
+
+    if(send_msg(sockFD, reqs.str()) == -1){
+        cerr << "send data to server failed" << endl;
+        return EXIT_FAILURE;
+    }
+
+    char buffer[100];
+    stringstream resp;
+    while(true){
+        int size = recv(sockFD, buffer, 100, 0);
+        if(size == -1){
+            cerr << "recv data failed" << endl;
+            return EXIT_FAILURE;
+        }
+        if(size == 0){
+            break;
+        }
+        resp << buffer;
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    cout << resp.str() << endl;
 
     return 0;
 }
